b3d59567f49fbc34b1ade01efe5047263f26c374
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2024-04-20 15:44:04 +0530

    send Email working md not working yet

--- app/api/sendWorkspaceInviteMail/route.ts
+++ app/api/sendWorkspaceInviteMail/route.ts
@@ -1,16 +1,18 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { Resend } from 'resend';
+import { backendUrl } from '@/utils/links';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_SECRET_KEY);
 
 export async function POST(request:any) {
   try {
-    const {email, workspace} = request.body;
+    const body = await request.json()
+    const {email, workspace} = body;
     const data = await resend.emails.send({
       from: process.env.SENDER_MAIL || "",
       to: email,
       subject: 'Hello world',
-      react: `Click on the link to join the workspace <a href="http://localhost:3000/api/getWorkspace/"${email}"/"${workspace}>Join</a>`,
+      react: `Click on the link to join the workspace <a href="${backendUrl}/api/getWorkspace/"${email}"/"${workspace}>Join</a>`,
     });
 
     return Response.json(data);
---
+++ app/mdx-page/page.tsx
@@ -0,0 +1,80 @@
+// ### visit now https://taskswift.netlify.app/
+
+// # Taskswift
+
+// This repository contains the source code for [Aeonaxy](https://aeonaxydev.netlify.app/), a web application built with Express.js and hosted on GitHub with the backend deployed on Render.
+
+// ## Getting Started
+
+// ### Prerequisites
+
+// - [Node.js](https://nodejs.org/) installed on your machine
+// - [npm](https://www.npmjs.com/) (Node Package Manager) installed on your machine
+// ```sql
+// CREATE TABLE VEDANT;
+// ```
+
+
+// ### Development
+// 1. Clone the repository:
+//     ```bash
+//     git clone https://github.com/Devvrat1010/aeonaxy
+//     ```
+// #### Backend 
+// 1. Go to the backend folder:
+//    ```bash
+//    cd backend
+//     ```
+   
+// 2. Install dependencies:
+//     ```bash
+//     npm install
+//     ```
+    
+// 3. To run the backend of the application in development mode, use the following command:
+//     ```bash
+//     npm start
+//     ```
+    
+// 4. Api endpoint :
+//     ```bash
+//     http://localhost:5000/
+//     ```
+    
+// #### Frontend 
+// 1. Go to the backend folder:
+//    ```bash
+//    cd frontend
+//     ```
+   
+// 2. Install dependencies:
+//     ```bash
+//     npm install
+//     ```
+    
+// 3. To run the frontend of the application in development mode, use the following command:
+//     ```bash
+//     npm run dev
+//     ```
+
+// ## Deployment
+
+// The backend of this application is deployed on [Render](https://render.com/). Ensure that you have the necessary configurations set up on your Render account.
+
+// ## Contributing
+
+// If you'd like to contribute to this project, please follow the [contribution guidelines](CONTRIBUTING.md).
+"use client"
+// import {createRoot} from 'react-dom/client'
+import Markdown from 'react-markdown'
+
+const markdown = '# Hi, *Pluto*!'
+
+// export default createRoot(document.body).render(<Markdown>{markdown}</Markdown>)
+export default function Test(){
+    return(
+        <h1>
+            Whyyyyyyyy
+        </h1>
+    )
+}
